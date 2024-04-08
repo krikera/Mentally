5c8fdf8ef322c2ec7f3fa65debe794f700ff8cc5
golubhattuk01 <148994632+golubhattuk01@users.noreply.github.com>
2024-04-08 11:33:13 +0530

    update nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,6 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <div className="text-red-500">hey red color</div>
       <h1 className="w-full text-3xl font-bold text-[#912BBC]">KernalKamn</h1>
       <ul className="hidden md:flex">
         <li className="p-4">Home</li>
